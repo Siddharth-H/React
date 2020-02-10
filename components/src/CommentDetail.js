8adf50824ee03cb67b43dc0ca8d9284aec58ee69
Siddharth-H <siddharthahatkar@gmail.com>
2020-02-10 05:24:54 -0700

    Prop exercise for image and content

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -7,7 +7,7 @@ const CommentDetail = props => {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()} />
+                <img alt="avatar" src={props.image} />
             </a>
             <div className="content">
                 <a href="/" className="author">
@@ -16,7 +16,7 @@ const CommentDetail = props => {
                 <div className="metadata">
     <span className="date">{props.timeAgo}</span>
                 </div>
-                <div className="text">Nice blog post</div>
+                <div className="text">{props.comment}</div>
             </div>
         </div>
     );
