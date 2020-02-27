31f6eddda0e2fb68a402401aae959f4bb6300cff
Siddharth-H <siddharthahatkar@gmail.com>
2020-02-27 03:25:06 -0700

    Updated states

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,7 +5,7 @@ class App extends React.Component{
     //specific to JS, not with react; works same as JS or Java constructor
     constructor(props){ // same props object
         super(props); // Required* ; to call base class React.Component constructor as it does the setup for react.
-        this.state = { lat: null }; //we put here most relevant data to our component; we will also set default value to the variables
+        this.state = { lat: null, errorMessage: '' }; //we put here most relevant data to our component; we will also set default value to the variables
     }
 
     //React says we have to define render!! Required**
@@ -14,12 +14,18 @@ class App extends React.Component{
             position => {
                 this.setState({ lat: position.coords.latitude });
             },
-            err => console.log(err),
+            err => {
+                // console.log(err)
+                this.setState({errorMessage: err.message})
+            }
             // props => {console.log(this.props)}
 
         );
         return(
-            <div>Latitude: {this.state.lat} </div>
+            <div>
+                Latitude: {this.state.lat}<br />
+                Error: {this.state.errorMessage}
+            </div>
         );
     }
 }
